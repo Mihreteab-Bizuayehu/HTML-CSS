f497fac53903cadb3a40601c280cf573eabb2c57
mihreteab-bizuayehu <lijmihret@gmail.com>
2025-03-24 16:08:09 +0300

    HTML and CSS simple Projects to improve html and css skills

---
+++ RESPONSIVE Layouts with CSS GRID/app.js
@@ -0,0 +1,7 @@
+let sidebar = document.querySelector(".sidebar");
+let header = document.querySelector(".header");
+
+function functionToggle() {
+    sidebar.classList.toggle('show')
+    header.classList.toggle("navbar");
+}
